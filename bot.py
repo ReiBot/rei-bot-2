81dbce372f0c47d86b9900b949c3e06b82a65cd5
oninbo <oninbo@gmail.com>
2018-07-13 22:55:43 +0300

    added answer_callback_query()

--- bot.py
+++ bot.py
@@ -226,6 +226,8 @@ def callback_inline(call: telebot.types.CallbackQuery) -> None:
         BOT.edit_message_reply_markup(chat_id=message.chat.id, message_id=message.message_id,
                                       reply_markup=keyboard)
 
+        BOT.answer_callback_query(call)
+
 
 # Remove webhook, it fails sometimes the set if there is a previous webhook
 BOT.remove_webhook()

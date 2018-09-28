95f105123ad82d40251fbdec5c099b8d59387ecf
oninbo <oninbo@gmail.com>
2018-09-28 19:43:31 +0300

    added separated testing modules for replying agents and text generating objects

---
+++ testing_reply_agents.py
@@ -0,0 +1,49 @@
+"""
+Module for testing replying agents (agents that produce reply to the text message)
+"""
+
+import os.path
+from typing import List, Dict
+import unittest
+
+import json_manager
+import agents
+
+AGENTS_REPLY_FUNCTIONS = [agents.CONVERSATION_CONTROLLER.proceed_input_message]
+
+TEST_NUMBERS = [1, 0, 2]
+
+
+class TestReplyAgents(unittest.TestCase):
+    """
+    Class for testing replying agents
+    """
+    def test_reply_agents(self) -> None:
+        """
+        Tests agents that can give replies on text messages
+        :return: None
+        """
+
+        for test_n in TEST_NUMBERS:
+            for agent_function in AGENTS_REPLY_FUNCTIONS:
+                test_file_name = os.path.join('data', 'tests', f'test{str(test_n)}.txt')
+                test_output_file_name = f'test_output_CONVERSATION_CONTROLLER_n_{str(test_n)}.txt'
+                if not (agent_function and test_file_name and test_output_file_name):
+                    return
+
+                # read lines of text as messages
+                with open(test_file_name, 'r', encoding='utf-8-sig') as test_file:
+                    messages = test_file.readlines()
+
+                # list with dictionaries that contain message text and reply
+                test_output: List[Dict[str, str]] = list()
+
+                for message in messages:
+                    test_output.append({"message": message,
+                                        "reply": agent_function(message)})
+
+                json_manager.write(test_output, test_output_file_name)
+
+
+if __name__ == '__main__':
+    unittest.main()
